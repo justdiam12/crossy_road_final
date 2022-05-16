1745f4bc67efc25af02d342c68c54500cd99d22f
justdiam12 <77951258+justdiam12@users.noreply.github.com>
2022-05-16 11:09:27 -0700

    Add files via upload

---
+++ Intermediate/Build/Mac/x86_64/UE4Editor/Inc/FinalProject/FinalProjectGameModeBase.generated.h
@@ -0,0 +1,93 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef FINALPROJECT_FinalProjectGameModeBase_generated_h
+#error "FinalProjectGameModeBase.generated.h already included, missing '#pragma once' in FinalProjectGameModeBase.h"
+#endif
+#define FINALPROJECT_FinalProjectGameModeBase_generated_h
+
+#define CPSC_386_Crossy_Road_master_2_Source_FinalProject_FinalProjectGameModeBase_h_15_SPARSE_DATA
+#define CPSC_386_Crossy_Road_master_2_Source_FinalProject_FinalProjectGameModeBase_h_15_RPC_WRAPPERS
+#define CPSC_386_Crossy_Road_master_2_Source_FinalProject_FinalProjectGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define CPSC_386_Crossy_Road_master_2_Source_FinalProject_FinalProjectGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAFinalProjectGameModeBase(); \
+	friend struct Z_Construct_UClass_AFinalProjectGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AFinalProjectGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/FinalProject"), NO_API) \
+	DECLARE_SERIALIZER(AFinalProjectGameModeBase)
+
+
+#define CPSC_386_Crossy_Road_master_2_Source_FinalProject_FinalProjectGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAFinalProjectGameModeBase(); \
+	friend struct Z_Construct_UClass_AFinalProjectGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AFinalProjectGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient | CLASS_Config), CASTCLASS_None, TEXT("/Script/FinalProject"), NO_API) \
+	DECLARE_SERIALIZER(AFinalProjectGameModeBase)
+
+
+#define CPSC_386_Crossy_Road_master_2_Source_FinalProject_FinalProjectGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AFinalProjectGameModeBase(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AFinalProjectGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AFinalProjectGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AFinalProjectGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AFinalProjectGameModeBase(AFinalProjectGameModeBase&&); \
+	NO_API AFinalProjectGameModeBase(const AFinalProjectGameModeBase&); \
+public:
+
+
+#define CPSC_386_Crossy_Road_master_2_Source_FinalProject_FinalProjectGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AFinalProjectGameModeBase(AFinalProjectGameModeBase&&); \
+	NO_API AFinalProjectGameModeBase(const AFinalProjectGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AFinalProjectGameModeBase); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AFinalProjectGameModeBase); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AFinalProjectGameModeBase)
+
+
+#define CPSC_386_Crossy_Road_master_2_Source_FinalProject_FinalProjectGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define CPSC_386_Crossy_Road_master_2_Source_FinalProject_FinalProjectGameModeBase_h_12_PROLOG
+#define CPSC_386_Crossy_Road_master_2_Source_FinalProject_FinalProjectGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	CPSC_386_Crossy_Road_master_2_Source_FinalProject_FinalProjectGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	CPSC_386_Crossy_Road_master_2_Source_FinalProject_FinalProjectGameModeBase_h_15_SPARSE_DATA \
+	CPSC_386_Crossy_Road_master_2_Source_FinalProject_FinalProjectGameModeBase_h_15_RPC_WRAPPERS \
+	CPSC_386_Crossy_Road_master_2_Source_FinalProject_FinalProjectGameModeBase_h_15_INCLASS \
+	CPSC_386_Crossy_Road_master_2_Source_FinalProject_FinalProjectGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define CPSC_386_Crossy_Road_master_2_Source_FinalProject_FinalProjectGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	CPSC_386_Crossy_Road_master_2_Source_FinalProject_FinalProjectGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	CPSC_386_Crossy_Road_master_2_Source_FinalProject_FinalProjectGameModeBase_h_15_SPARSE_DATA \
+	CPSC_386_Crossy_Road_master_2_Source_FinalProject_FinalProjectGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	CPSC_386_Crossy_Road_master_2_Source_FinalProject_FinalProjectGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	CPSC_386_Crossy_Road_master_2_Source_FinalProject_FinalProjectGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> FINALPROJECT_API UClass* StaticClass<class AFinalProjectGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID CPSC_386_Crossy_Road_master_2_Source_FinalProject_FinalProjectGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
